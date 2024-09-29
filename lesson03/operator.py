2574c61be5c73b0077ef38c94ecdba65b1d90357
YannKamche <yannkamche@gmail.com>
2024-09-29 15:00:15 +0100

    operators

---
+++ lesson03/operator.py
@@ -0,0 +1,17 @@
+# operators: Symbols used to perform operations on 
+#            values and the variables that hold those values.
+
+# A program to display whether a day will be sunny or rainy based on the 
+# temperature
+temperature = 24;
+
+# Conditional operator in Python
+if temperature < 30 :
+    print("It's a rainy day");
+else:
+    print("It is a sunny day");
+    
+print('');
+
+# The ternary operator in Python
+print("It will rain") if temperature < 30 else print("It will not rain");
